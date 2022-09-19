7ae8df61d6b1a13c2c2765dbc6f756a52994ccb3
Adrial <yonnis.peyrous@gmail.com>
2022-09-19 19:41:13 +0200

    caroussel v1

--- index.js
+++ index.js
@@ -5,3 +5,30 @@
         document.documentElement.setAttribute('class','stuck');
     };
 })).observe(document.querySelector('.trigger'));
+
+
+// -------------------Slider présentation pages annexes--------------------
+// --------------------------by Peyrous Yonnis-----------------------------
+
+let img_slider = document.getElementsByClassName('img_slider'); 
+
+let etape = 0;
+
+let nbr_img = img_slider.length;
+
+function enleverActiveImages() {        // met les images que l'on ne veut pas visible avec une opacité de 0 (CSS)
+    for(let i = 0 ; i < nbr_img ; i++) {
+        img_slider[i].classList.remove('active');
+    }
+}
+
+setInterval(function() {                // passage auto à l'image suivante
+    etape++;
+    if(etape >= nbr_img) {
+        etape = 0;
+    }
+    enleverActiveImages();
+    img_slider[etape].classList.add('active');
+}, 8000)
+
+// ------------------------------------------------------------------------
